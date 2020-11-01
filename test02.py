1c189a6934ae0ab69e430759e8bebdc0e6eaafd9
WHUTyuen <59003432+WHUTyuen@users.noreply.github.com>
2020-11-01 17:43:38 +0800

    Add files via upload

---
+++ test02.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CenterLoss(nn.Module):
+
+    def __init__(self,cls_num,feature_num):
+        super().__init__()
+
+        self.cls_num = cls_num
+        self.center = nn.Parameter(torch.randn(cls_num,feature_num))
+
+    def forward(self,xs,ys):
+        xs = F.normalize(xs)
+        center_exp = self.center.index_select(dim=0,index=ys.long())
+        count = torch.histc(ys,bins=self.cls_num,min=0,max=self.cls_num-1)
+        count_exp = count.index_select(dim=0,index=ys.long())
+        return torch.sum(torch.div(torch.sqrt(torch.sum(torch.pow(2,xs-center_exp),dim=1)),count_exp))
+
+
+class MainNet(nn.Module):
+    def __init__(self):
+        self.hidden_layer = nn.Sequential(
+            nn.Linear(784,120),
+            nn.LeakyReLU(),
+            nn.Linear(120,2)
+        )
+        self.output_layer = nn.Sequential(
+            nn.Linear(2,10)
+        )
+
+        self.center_loss_layer = CenterLoss(10,2)
+        self.crossEntropy = nn.CrossEntropyLoss()
+    def forward(self,xs):
+        features = self.hidden_layer(xs)
+        outputs = self.output_layer(features)
+        return features,outputs
+    def getloss(self,outputs,features,labels):
+        loss_cls = self.crossEntropy(outputs,labels)
+        loss_center = self.center_loss_layer(features,labels)
+        loss = loss_cls+loss_center
+        return loss
+
+
---
+++ test03.py
@@ -0,0 +1,5 @@
+import torch
+
+a = torch.Tensor([4,6])
+b = torch.Tensor([2,3])
+print(torch.div(b,a))
